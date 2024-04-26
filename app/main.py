69b61f99e2941c3ea593c08281cbf939354f64f3
ray-chunkit-chung <ray.chunkit.chung@gmail.com>
2024-04-26 18:12:06 +0900

    add a model predict placeholder

--- app/main.py
+++ app/main.py
@@ -1,15 +1,40 @@
+from contextlib import asynccontextmanager
 from fastapi import FastAPI
 import os
 
-app = FastAPI()
+
+async def fake_answer_to_everything_ml_model(x: int):
+    return x * 42
+
+
+ml_models = {}
+
+
+@asynccontextmanager
+async def lifespan(app: FastAPI):
+    # Load the ML model
+    ml_models["answer_to_everything"] = fake_answer_to_everything_ml_model
+    yield
+    # Clean up the ML models and release the resources
+    ml_models.clear()
+
+
+app = FastAPI(lifespan=lifespan)
 
 # heroku port setting
-PORT = os.environ.get('PORT', 8000)
+PORT = os.environ.get("PORT", 8000)
 
 
 @app.get("/")
 async def root():
-    return {"Nginx": "I'm alive over TvT v9000.90 T.T yametekudastop!!!"}
+    return {"Nginx": "I'm alive over TvT v9000.50 T.T yametekudastop!!!"}
+
+
+@app.post("/predict")
+async def predict(x: int):
+    result = ml_models["answer_to_everything"](x)
+    return {"result": result}
+
 
 # heroku port setting
 if __name__ == "__main__":
