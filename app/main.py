1bcd68b96dac3279d69d45d021304043eb635f92
ray-chunkit-chung <ray.chunkit.chung@gmail.com>
2024-04-27 18:56:40 +0900

    add real ml model endpoint placeholder

--- app/main.py
+++ app/main.py
@@ -4,17 +4,8 @@ from fastapi import FastAPI
 import uvicorn
 import os
 
-
-# Input Data Validation
-class ModelInput(BaseModel):
-    IntegerInput: int
-
-
-async def fake_ml_model(data: dict) -> dict:
-    prediction = data.get("IntegerInput", 0) * 42
-    return {"prediction": prediction}
-
-
+######################################################
+# Define Application Lifecycle
 ml_models = {}
 
 
@@ -22,6 +13,7 @@ ml_models = {}
 async def lifespan(app: FastAPI):
     # Load the ML model
     ml_models["fake_ml_model"] = fake_ml_model
+    ml_models["text_classifier"] = text_classifier
     yield
     # Clean up the ML models and release the resources
     ml_models.clear()
@@ -33,15 +25,43 @@ app = FastAPI(lifespan=lifespan)
 PORT = os.environ.get("PORT", 8000)
 
 
+######################################################
+# Input Data Validation
+class FakeModelInput(BaseModel):
+    IntegerInput: int
+
+
+class ModelInput(BaseModel):
+    IntegerInput: int
+
+
+######################################################
+# Prediction Logic
+
+
+async def fake_ml_model(data: dict) -> dict:
+    prediction = data.get("IntegerInput", 0) * 42
+    return {"prediction": prediction}
+
+
+async def text_classifier(data: dict) -> dict:
+    prediction = data.get("IntegerInput", 0) * 42
+    return {"prediction": prediction}
+
+
+######################################################
+# Endpoints
+
+
 @app.get("/")
 async def root():
-    return {"Nginx": "I'm alive over TvT v9000.47 T.T yametekudastop!!!"}
+    return {"Nginx": "I'm alive over TvT v9000.46 T.T yametekudastop!!!"}
 
 
 @app.post("/predict")
 async def predict(model_input: ModelInput):
     data = dict(model_input)
-    result = await ml_models["fake_ml_model"](data)
+    result = await ml_models["text_classifier"](data)
     return {"result": result}
 
 
